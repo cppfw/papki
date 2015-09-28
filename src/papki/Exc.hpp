420bad2706e4186d53e340f67aea4f0d2b955dbd
Ivan Gagis <igagis@gmail.com>
2015-09-28 21:59:08 +0300

    missing file

---
+++ src/papki/Exc.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <utki/Exc.hpp>
+
+
+
+namespace papki{
+
+/**
+ * @brief Basic exception class.
+ */
+class Exc : public utki::Exc{
+public:
+	/**
+	 * @brief Constructor.
+	 * @param descr - human readable description of the error.
+	 */
+	Exc(const std::string& descr) :
+			utki::Exc(std::string("[papki::Exc]: ") + descr)
+	{}
+};
+
+/**
+ * @brief Illegal state exception.
+ * This exception is usually thrown when trying to perform some operation on the
+ * object while the object is in inappropriate state for that operation. For example,
+ * when trying to read from file while it is not opened.
+ */
+class IllegalStateExc : public Exc{
+public:
+	/**
+	 * @brief Constructor.
+	 * @param descr - human readable description of the error.
+	 */
+	IllegalStateExc(const std::string& descr = "Illegal opened/closed state") :
+			Exc(descr)
+	{}
+};
+
+}
