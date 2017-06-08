f12ba7e263938e5eea0f22ed85c21e63a18c36ff
Ivan Gagis <igagis@gmail.com>
2017-06-08 15:24:50 +0300

    disable cygwin build for now

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -18,7 +18,7 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
 }
 
 
-"%VS140COMNTOOLS%\VsMSBuildCmd.bat"
+"%VS141COMNTOOLS%\VsMSBuildCmd.bat"
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Debug /p:Platform=x86 /v:minimal /nologo
 If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Release /p:Platform=x86 /v:minimal /nologo
