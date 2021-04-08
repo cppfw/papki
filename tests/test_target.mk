d2797bc305ab02e3240cb34ef6f45b6205dab1d6
Ivan Gagis <igagis@gmail.com>
2021-04-08 15:10:52 +0300

    configs

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -1,10 +1,10 @@
 ifeq ($(os),windows)
     # to avoid /C converted to C:\ need to escape it as //C
-    this_test_cmd := (cd $(d) && cmd //C 'set PATH=../../src;%PATH% && $$(notdir $$^)')
+    this_test_cmd := (cd $(d) && cmd //C 'set PATH=../../src/out/$(c);%PATH% && out/$(c)/$$(notdir $$^)')
 else ifeq ($(os),macosx)
-    this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src ./$$(notdir $$^))
+    this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c) out/$(c)/$$(notdir $$^))
 else ifeq ($(os),linux)
-    this_test_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src ./$$(notdir $$^))
+    this_test_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/out/$(c) out/$(c)/$$(notdir $$^))
 else
     $(error "Unknown OS")
 endif
@@ -14,26 +14,23 @@ this_test := $(word $(words $(this_dirs)), $(this_dirs))
 
 define this_rule
 test:: $(prorab_this_name)
-	@myci-running-test.sh $(this_test)
-	$(prorab_echo)$(this_test_cmd) || myci-error.sh "test failed"
-	@myci-passed.sh
+$(.RECIPEPREFIX)@myci-running-test.sh $(this_test)
+$(.RECIPEPREFIX)$(a)$(this_test_cmd) || myci-error.sh "test failed"
+$(.RECIPEPREFIX)@myci-passed.sh
 endef
 $(eval $(this_rule))
 
-
-
 ifeq ($(os),windows)
     # to avoid /C converted to C:\ need to escape it as //C
-    this_gdb_cmd := (cd $(d) && cmd //C 'set PATH=../../src;%PATH% && gdb $$(notdir $$^)')
+    this_gdb_cmd := (cd $(d) && cmd //C 'set PATH=../../src/out/$(c);%PATH% && gdb out/$(c)/$$(notdir $$^)')
 else ifeq ($(os),macosx)
-    this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src gdb ./$$(notdir $$^))
+    this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c) gdb out/$(c)/$$(notdir $$^))
 else ifeq ($(os),linux)
-    this_gdb_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src gdb ./$$(notdir $$^))
+    this_gdb_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/out/$(c) gdb out/$(c)/$$(notdir $$^))
 endif
-
 
 define this_rule
 gdb:: $(prorab_this_name)
-	$(prorab_echo)$(this_gdb_cmd)
+$(.RECIPEPREFIX)$(a)$(this_gdb_cmd)
 endef
 $(eval $(this_rule))
