4f5deafa2f6766f138d4ef7a00752ec9f613f54c
Ivan Gagis <igagis@gmail.com>
2021-04-13 23:15:32 +0300

    improve makefile

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -1,19 +1,7 @@
-prorab-lib-path-run = \
-        $(if $(filter $(os),windows), \
-                (cd $(d) && cmd //C 'set PATH=$1;%PATH%; $(subst /,\\,$2)'), \
-                $(if $(filter $(os),macosx), \
-                        (export DYLD_LIBRARY_PATH=$1; cd $(d) && $2), \
-                        $(if $(filter $(os),linux), \
-                                (export LD_LIBRARY_PATH=$1; cd $(d) && $2), \
-                                $(error "unknown OS") \
-                            ) \
-                    ) \
-            )
+include prorab-test.mk
 
-define this_rule
-test:: $(prorab_this_name)
-$(.RECIPEPREFIX)@myci-running-test.sh $(notdir $(abspath $(d)))
-$(.RECIPEPREFIX)$(a)$(call prorab-lib-path-run,../../src/out/$(c),out/$(c)/$$(notdir $$^)) || myci-error.sh "test failed"
-$(.RECIPEPREFIX)@myci-passed.sh
-endef
-$(eval $(this_rule))
+this_test_cmd := $(prorab_this_name)
+this_test_deps := $(prorab_this_name)
+this_test_ld_path := ../../src/out/$(c)
+
+$(eval $(prorab-test))
