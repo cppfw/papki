5c6664a0d428481678075e32ae30c5231a6807ed
Ivan Gagis <igagis@gmail.com>
2021-04-13 21:33:36 +0300

    makefile refactoring

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -1,36 +1,19 @@
-ifeq ($(os),windows)
-    # to avoid /C converted to C:\ need to escape it as //C
-    this_test_cmd := (cd $(d) && cmd //C 'set PATH=../../src/out/$(c);%PATH% && out\$(c)\$$(notdir $$^)')
-else ifeq ($(os),macosx)
-    this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c) out/$(c)/$$(notdir $$^))
-else ifeq ($(os),linux)
-    this_test_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/out/$(c) out/$(c)/$$(notdir $$^))
-else
-    $(error "Unknown OS")
-endif
-
-this_dirs := $(subst /, ,$(d))
-this_test := $(word $(words $(this_dirs)), $(this_dirs))
+prorab-lib-path-run = (cd $(d) && \
+        $(if $(filter $(os),windows), \
+                cmd //C 'set PATH=$1;%PATH% && $2', \
+                $(if $(filter $(os),macosx), \
+                        DYLD_LIBRARY_PATH=$1 $2, \
+                        $(if $(filter $(os),linux), \
+                                LD_LIBRARY_PATH=$1 $2, \
+                                $(error "unknown OS") \
+                            ) \
+                    ) \
+            ) )
 
 define this_rule
 test:: $(prorab_this_name)
-$(.RECIPEPREFIX)@myci-running-test.sh $(this_test)
-$(.RECIPEPREFIX)$(a)$(this_test_cmd) || myci-error.sh "test failed"
+$(.RECIPEPREFIX)@myci-running-test.sh $(notdir $(abspath $(d)))
+$(.RECIPEPREFIX)$(a)$(call prorab-lib-path-run,../../src/out/$(c),out/$(c)/$$(notdir $$^)) || myci-error.sh "test failed"
 $(.RECIPEPREFIX)@myci-passed.sh
-endef
-$(eval $(this_rule))
-
-ifeq ($(os),windows)
-    # to avoid /C converted to C:\ need to escape it as //C
-    this_gdb_cmd := (cd $(d) && cmd //C 'set PATH=../../src/out/$(c);%PATH% && gdb out\$(c)\$$(notdir $$^)')
-else ifeq ($(os),macosx)
-    this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c) gdb out/$(c)/$$(notdir $$^))
-else ifeq ($(os),linux)
-    this_gdb_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/out/$(c) gdb out/$(c)/$$(notdir $$^))
-endif
-
-define this_rule
-gdb:: $(prorab_this_name)
-$(.RECIPEPREFIX)$(a)$(this_gdb_cmd)
 endef
 $(eval $(this_rule))
