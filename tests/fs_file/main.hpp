cc0be488a00951661e8b85c07d264d516e1ad8fd
Ivan Gagis <igagis@gmail.com>
2021-06-06 01:01:58 +0300

    enable list dir test

--- tests/fs_file/main.hpp
+++ tests/fs_file/main.hpp
@@ -4,12 +4,9 @@
 
 #include "tests.hpp"
 
-
 inline void TestTingFSFile(){
 	TestSeekForward::Run();
-	// TODO: re-enable the test when bug in qemu is fixed (https://bugs.launchpad.net/qemu/+bug/1805913)
-	utki::log([](auto&o){o << "WARNING: TestListDirContents test is not run due to bug in qemu" << std::endl;});
-	// TestListDirContents::Run();
+	TestListDirContents::Run();
 	TestHomeDir::Run();
 	TestLoadWholeFileToMemory::Run();
 }
