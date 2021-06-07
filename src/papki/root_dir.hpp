cc6d509a3dd49e11425d37c238b3266924fff483
Ivan Gagis <igagis@gmail.com>
2021-06-07 23:21:59 +0300

    renamings root_dir

--- src/papki/root_dir.hpp
+++ src/papki/root_dir.hpp
@@ -8,27 +8,27 @@ namespace papki{
 
 //TODO: doxygen
 class root_dir : public file{
-	std::unique_ptr<file> baseFile;
-	std::string rootDir;
+	std::unique_ptr<file> base_file;
+	std::string root_directory;
 	
 public:
 	/**
-	 * @param baseFile - a file to wrap.
-	 * @param rootDir - path to the root directory to set. It should have trailing '/' character.
+	 * @param base_file - a file to wrap.
+	 * @param root_directory - path to the root directory to set. It should have trailing '/' character.
 	 */
-	root_dir(std::unique_ptr<file> baseFile, const std::string& rootDir) :
-			baseFile(std::move(baseFile)),
-			rootDir(rootDir)
+	root_dir(std::unique_ptr<file> base_file, const std::string& root_directory) :
+			base_file(std::move(base_file)),
+			root_directory(root_directory)
 	{
-		if(!this->baseFile){
+		if(!this->base_file){
 			throw std::invalid_argument("root_dir(): passed in base file pointer is null");
 		}
-		this->file::set_path_internal(std::string(this->baseFile->path()));
-		this->baseFile->set_path(this->rootDir + this->path());
+		this->file::set_path_internal(std::string(this->base_file->path()));
+		this->base_file->set_path(this->root_directory + this->path());
 	}
 	
-	static std::unique_ptr<const root_dir> make(std::unique_ptr<const file> baseFile, const std::string& rootDir){
-		return utki::make_unique<const root_dir>(std::unique_ptr<file>(const_cast<file*>(baseFile.release())), rootDir);
+	static std::unique_ptr<const root_dir> make(std::unique_ptr<const file> base_file, const std::string& root_directory){
+		return utki::make_unique<const root_dir>(std::unique_ptr<file>(const_cast<file*>(base_file.release())), root_directory);
 	}
 	
 	root_dir(const root_dir&) = delete;
@@ -37,51 +37,51 @@ public:
 private:
 	void set_path_internal(std::string&& pathName)const override{
 		this->file::set_path_internal(std::move(pathName));
-		this->baseFile->set_path(this->rootDir + this->path());
+		this->base_file->set_path(this->root_directory + this->path());
 	}
 	
 	void open_internal(mode io_mode)override{
-		this->baseFile->open(io_mode);
+		this->base_file->open(io_mode);
 	}
 	
 	void close_internal()const noexcept override{
-		this->baseFile->close();
+		this->base_file->close();
 	}
 	
 	std::vector<std::string> list_dir(size_t maxEntries = 0)const override{
-		return this->baseFile->list_dir(maxEntries);
+		return this->base_file->list_dir(maxEntries);
 	}
 	
 	size_t read_internal(utki::span<uint8_t> buf)const override{
-		return this->baseFile->read(buf);
+		return this->base_file->read(buf);
 	}
 	
 	size_t write_internal(utki::span<const uint8_t> buf)override{
-		return this->baseFile->write(buf);
+		return this->base_file->write(buf);
 	}
 	
 	size_t seek_forward_internal(size_t numBytesToSeek)const override{
-		return this->baseFile->seek_forward(numBytesToSeek);
+		return this->base_file->seek_forward(numBytesToSeek);
 	}
 	
 	size_t seek_backward_internal(size_t numBytesToSeek)const override{
-		return this->baseFile->seek_backward(numBytesToSeek);
+		return this->base_file->seek_backward(numBytesToSeek);
 	}
 	
 	void rewind_internal()const override{
-		this->baseFile->rewind();
+		this->base_file->rewind();
 	}
 	
 	void make_dir()override{
-		this->baseFile->make_dir();
+		this->base_file->make_dir();
 	}
 	
 	bool exists()const override{
-		return this->baseFile->exists();
+		return this->base_file->exists();
 	}
 	
 	std::unique_ptr<file> spawn()override{
-		return utki::make_unique<root_dir>(this->baseFile->spawn(), this->rootDir);
+		return utki::make_unique<root_dir>(this->base_file->spawn(), this->root_directory);
 	}
 };
 
