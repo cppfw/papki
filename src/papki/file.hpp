3ed81517f48282bba2e299cb8f6ec1d764408090
Ivan Gagis <igagis@gmail.com>
2021-06-09 11:29:36 +0300

    release 1.0.101

--- src/papki/file.hpp
+++ src/papki/file.hpp
@@ -76,7 +76,15 @@ public:
 	 * @brief Set the path for this file instance.
 	 * @param path_name - the path to a file or directory.
 	 */
-	void set_path(const std::string& path_name)const{
+	void set_path(std::string_view path_name)const{
+		this->set_path(std::string(path_name));
+	}
+
+	/**
+	 * @brief Set the path for this file instance.
+	 * @param path_name - the path to a file or directory.
+	 */
+	void set_path(const char* path_name)const{
 		this->set_path(std::string(path_name));
 	}
 
