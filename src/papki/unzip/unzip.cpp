85169b794f4ba69173322ce58528d10746c3de11
Ivan Gagis <ivan.gagis@ge.com>
2023-08-24 16:12:53 +0300

    format

--- src/papki/unzip/unzip.cpp
+++ src/papki/unzip/unzip.cpp
@@ -545,7 +545,7 @@ local void unzlocal_DosDateToTmuDate(uLong ulDosDate, tm_unz* ptm) // NOLINT
 	uLong uDate; // NOLINT
 	uDate = (uLong)(ulDosDate >> 16); // NOLINT
 	ptm->tm_mday = (uInt)(uDate & 0x1f); // NOLINT
-	ptm->tm_mon = (uInt)((((uDate)&0x1E0) / 0x20) - 1); // NOLINT
+	ptm->tm_mon = (uInt)((((uDate) & 0x1E0) / 0x20) - 1); // NOLINT
 	ptm->tm_year = (uInt)(((uDate & 0x0FE00) / 0x0200) + 1980); // NOLINT
 	// NOLINT
 	ptm->tm_hour = (uInt)((ulDosDate & 0xF800) / 0x800); // NOLINT
