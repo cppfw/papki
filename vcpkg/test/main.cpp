467e69f5bee1dc8a8d11b4108eb83d6d039b13eb
Ivan Gagis <igagis@gmail.com>
2024-11-23 16:08:08 +0200

    vcpkg: cleanup

--- vcpkg/test/main.cpp
+++ vcpkg/test/main.cpp
@@ -1,7 +1,7 @@
 #include <papki/fs_file.hpp>
 
 int main(int argc, const char** argv){
-    papki::fs_file dir(".");
+    papki::fs_file dir("./");
 
     std::cout << "num files = " << dir.list_dir().size() << std::endl;
 
