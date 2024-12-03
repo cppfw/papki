4a4f504fb98ebc0d57eb46e0efd454db64c9a285
Ivan Gagis <igagis@gmail.com>
2024-12-03 10:29:55 +0200

    conan: macos

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -21,8 +21,9 @@ class PapkiConan(ConanFile):
 		self.requires("utki/[>=1.1.202]@cppfw/main", transitive_headers=True)
 		self.requires("zlib/[>=1.2.0]")
 
-	# def build_requirements(self):
-	# 	self.requires("tst/[>=0.3.29]@cppfw/main", visible=False)
+	def build_requirements(self):
+		self.tool_requires("prorab/[>=2.0.27]@cppfw/main")
+		self.tool_requires("prorab-extra/[>=0.2.57]@cppfw/main")
 
 	def config_options(self):
 		if self.settings.os == "Windows":
@@ -52,8 +53,8 @@ class PapkiConan(ConanFile):
 		git.run("submodule update --init --remote --depth 1")
 
 	def build(self):
-		self.run("make lint=off")
-		self.run("make lint=off test")
+		self.run("make $MAKE_INCLUDE_DIRS_ARG lint=off")
+		self.run("make $MAKE_INCLUDE_DIRS_ARG lint=off test")
 
 	def package(self):
 		src_dir = os.path.join(self.build_folder, "src")
